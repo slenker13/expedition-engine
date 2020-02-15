dcc962de010103829d14e81a6b23e26398809fb3
slenker13 <stevenblenker@gmail.com>
2020-02-15 00:08:04 -0500

    chore: Add headers to Expedition.hpp

--- include/Expedition.hpp
+++ include/Expedition.hpp
@@ -1,6 +1,11 @@
 #ifndef EXPEDITION_HPP
 #define EXPEDITION_HPP
 
+// Expedition Engine headers
+#include "Texture.hpp"
+#include "Timer.hpp"
+#include "Window.hpp"
+
 namespace Expedition {
     // Initializes SDL systems
     bool init();
